da2445d0ff216a0e70382be1ebbfb2eb5f3c640b
FroznMine <pesafrre@arcor.de>
2015-02-18 21:09:49 +0100

    Create template page
    Just a simple template unedited page to test whether it works

---
+++ FroznMine/Willkommen_files/Willkommen.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20150218
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Willkommen_files/WillkommenMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
